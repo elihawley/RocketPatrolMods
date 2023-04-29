0a6ba479b60589d5b15513fb31668080e33e1588
elihawley <104234916+elihawley@users.noreply.github.com>
2023-04-29 11:33:06 -0700

    Initial upload

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,43 @@
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);
+        this.isFiring = false;
+        this.moveSpeed = 2;
+
+        this.sfxRocket = scene.sound.add('sfx_rocket');
+    }
+
+    update() {
+        // moving
+        if (!this.isFiring) {
+            if (keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+
+        // firing
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();
+        }
+
+        // if firing, move up
+        if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+
+        // reset on miss
+        if (this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+}
